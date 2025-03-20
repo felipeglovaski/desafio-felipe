7a68bf042e3d46007d7dcd8ca3fb1169bd54d604
felipeglovaski <felipeglovaski@gmail.com>
2025-03-20 10:29:27 -0300

    finalizado

--- src/index.js
+++ src/index.js
@@ -1,33 +1,33 @@
-var nome = "Hulk"
-var experiencia = 4800
+var nome = "Hulk";
+var experiencia = 4800;
 
-switch (experiencia){
+switch (true){
     
-    case (experiencia <= 1000) :
+    case experiencia <= 1000:
         console.log("O personagem" + nome + "esta no nível de Ferro")
         break
 
-    case (experiencia > 1001 && experiencia <= 2000) :
+    case experiencia > 1001 && experiencia <= 2000:
         console.log("O personagem" + nome + "esta no nível de Bronze")
         break
      
-    case (experiencia > 2001 && experiencia <= 5000) :
-        console.log("O personagem" + nome + "esta no nível de Prata")
+    case experiencia > 2001 && experiencia <= 5000:
+        console.log("O personagem " + nome + " esta no nível de Prata")
         break
 
-    case (experiencia > 5001 && experiencia <= 7000) :
+    case experiencia > 5001 && experiencia <= 7000:
         console.log("O personagem" + nome + "esta no nível de Ouro")
         break  
 
-    case (experiencia > 7001 && experiencia <= 8000) :
+    case experiencia > 7001 && experiencia <= 8000:
         console.log("O personagem" + nome + "esta no nível de Platina")
         break
         
-    case (experiencia > 8001 && experiencia <= 9000) :
+    case experiencia > 8001 && experiencia <= 9000:
         console.log("O personagem" + nome + "esta no nível de Ascendente")
         break
         
-    case (experiencia > 9001 && experiencia <= 10000) :
+    case experiencia > 9001 && experiencia <= 10000:
         console.log("O personagem" + nome + "esta no nível de Imortal")
         break
         
